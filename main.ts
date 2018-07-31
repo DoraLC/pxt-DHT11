e4ebea4d6857f9b4fcea4ed13a100f4b5c5a64a6
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-07-31 10:15:27 +0800

    update

--- main.ts
+++ main.ts
@@ -7,9 +7,8 @@ enum dht11type {
     humidity
 }
 
-//% color=#F6421B icon="\uf2c9" block="dht11"
+//% color=#F6421B icon="\uf2c9" block="DHT11"
 namespace DHT11 {
-    let temp = 0
     let pin = DigitalPin.P0;
     function signal_dht11(pin: DigitalPin): void {
         pins.digitalWritePin(pin, 0)
@@ -21,15 +20,15 @@ namespace DHT11 {
 
     /**
      * Set up the sensor and start reading data.
-     * 
+     *
      */
-    //% block="Start reading data from pin %pin_arg"
+    //% block="DHT11 pin %pin_arg|data %data_type"
     //% blockId=reading_dht11_data
     //% pin_arg.fieldEditor="gridpicker" pin_arg.fieldOptions.columns=5
     //% pin_arg.fieldOptions.tooltips="false"
     //% weight=0
-    export function dht11_read(pin_arg: DigitalPin): void {
-        basic.pause(1000)
+    export function dht11_read(pin_arg: DigitalPin, data_type: dht11type): number {
+        basic.pause(1500)
         pin = pin_arg;
         signal_dht11(pin);
 
@@ -51,22 +50,13 @@ namespace DHT11 {
                 value = value + (1 << (31 - i));
             }
         }
-        temp = value
-    }
-
-    /** Show specific data after reading from thr DHT11.  
-     *  
-    */
-    //% block="DHT11 get %data_type"
-    //% blockId=showing_dht11_data
-    export function showingData(data_type: dht11type): number {
         switch (data_type) {
             case 0:
-                return (temp & 0x0000ff00) >> 8;
+                return (value & 0x0000ff00) >> 8;
             case 1:
-                return ((temp & 0x0000ff00) >> 8) * 9 / 5 + 32;
+                return ((value & 0x0000ff00) >> 8) * 9 / 5 + 32;
             case 2:
-                return temp >> 24;
+                return value >> 24;
             default:
                 return 0;
         }
